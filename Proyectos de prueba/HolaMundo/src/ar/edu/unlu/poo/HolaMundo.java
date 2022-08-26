0d8df4c1669bf66e106e2e1df5b3b7bc000dbe24
egreric <eric.egr@gmail.com>
2022-08-26 19:49:48 -0300

    Agregando primer proyecto Hola Mundo

---
+++ Proyectos de prueba/HolaMundo/src/ar/edu/unlu/poo/HolaMundo.java
@@ -0,0 +1,16 @@
+package ar.edu.unlu.poo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo!");
+		int a = 5;
+		boolean nombreCambiado = true;
+		imprimirDespedida();
+		nombreCambiado = false;
+	}
+
+	public static void imprimirDespedida() {
+		System.out.println("Chau!");
+	}
+}
